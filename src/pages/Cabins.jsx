725db679669c9e42ecb669b050bcc5d675af636c
ilarumjarus <ilarumjarus@gmail.com>
2026-02-18 08:51:58 +0530

    Fecthing Cabins Data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,32 @@
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Cabins() {
+  //   useEffect(() => {
+  //     async function loadCabinsData() {
+  //       const cabins = await getCabins();
+  //       console.log("cabins", cabins);
+  //     }
+  //     loadCabinsData();
+  //   }, []);
+
+  // useEffect(() => {
+  //   getCabins()
+  //     .then((data) => console.log(data))
+  //     .catch((error) => console.log(error));
+  // }, []);
+
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p> Filter/Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
